5742a7bfacd1a3605ab70b77fee1bf48165d8654
crystilonz <64011729@kmitl.ac.th>
2021-10-08 22:21:10 +0700

    Addition, Subtraction, Multiplication, Exponentiation Memory Allocations for 2D matrices Changed matrix into a struct

--- functions.c
+++ functions.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef float Matrix[3][3];
+typedef struct
+{
+    int row;
+    int col;
+    float** index;
+}Matrix;
 
-void getMatrix(Matrix);
-void printMatrix(Matrix);
 float getfloat();
-void copyMatrix(Matrix, Matrix);
-void transpose(Matrix, Matrix);
+int getint();
+void iniMatrix(Matrix*, int, int);
+int checkMatrix(Matrix);
+void getMatrix(Matrix*);
+void printMatrix(Matrix);
+void addMatrix(Matrix, Matrix, Matrix*);
+void subMatrix(Matrix, Matrix, Matrix*);
+void scalarMatrix(Matrix, float, Matrix*);
+void copyMatrix(Matrix, Matrix*);
+void powerMatrix(Matrix, int, Matrix*);
+
 
 float getfloat()
 {
@@ -32,61 +44,136 @@ int getint()
     {
         bufferstring[i] = (char) character;
         ++i;
+        if(i >= 18)
+            break;
     }
     bufferstring[i] = '\0';
-    return strtol(bufferstring, NULL, 10);
+    return (int) strtol(bufferstring, NULL, 10);
 }
 
-void getMatrix(Matrix input)
+int checkMatrix(Matrix input)
 {
-    printf("Input a 3x3 matrix by rows using Aij\n");
-    printf("A11: ");
-    input[0][0] = getfloat();
+    if(input.row == 0 || input.col == 0 || input.index == NULL)
+        return 0;
 
-    printf("A12: ");
-    input[0][1] = getfloat();
+    for(int row = 0; row < input.row; row++)
+    {
+        if(input.index[row] == NULL)
+            return 0;
+    }
 
-    printf("A13: ");
-    input[0][2] = getfloat();
+    return 1;
+}
 
-    printf("A21: ");
-    input[1][0] = getfloat();
+void iniMatrix(Matrix* ini, int row, int col)
+{
+    ini->row = row;
+    ini->col = col;
+    ini->index = (float**) malloc(row * sizeof(float*));
+    for(int i = 0; i < row; ++i)
+    {
+        ini->index[i] = (float*) malloc(col * sizeof(float));
+    }
+    if(!checkMatrix(*ini))
+    {
+        printf("Initialisation failed! Cannot allocate memory");
+    }
+}
 
-    printf("A22: ");
-    input[1][1] = getfloat();
-
-    printf("A23: ");
-    input[1][2] = getfloat();
-
-    printf("A31: ");
-    input[2][0] = getfloat();
-
-    printf("A32: ");
-    input[2][1] = getfloat();
-
-    printf("A33: ");
-    input[2][2] = getfloat();
-
-    printf("\n");
+void getMatrix(Matrix* input)
+{
+    printf("Specify number of rows: ");
+    int row = getint();
+    printf("Specify number of columns: ");
+    int col = getint();
+    iniMatrix(input, row, col);
+    printf("Input a %ix%i matrix by rows using Aij\n", input->row, input->col);
+    for(int current_row = 0; current_row < row; ++current_row)
+    {
+        for(int current_col = 0; current_col < col; ++current_col)
+        {
+            printf("A%i%i = ", current_row + 1, current_col + 1);
+            input->index[current_row][current_col] = getfloat();
+        }
+    }
 }
 
 void printMatrix(Matrix matrix)
 {
-    printf("\n[ %5.1f, %5.1f, %5.1f ]\n", matrix[0][0], matrix[0][1], matrix[0][2]);
-    printf("[ %5.1f, %5.1f, %5.1f ]\n", matrix[1][0], matrix[1][1], matrix[1][2]);
-    printf("[ %5.1f, %5.1f, %5.1f ]\n", matrix[2][0], matrix[2][1], matrix[2][2]);
+    printf("\n");
+    for(int current_row = 0; current_row < matrix.row; ++current_row)
+    {
+        printf("[");
+        for(int current_col = 0; current_col < matrix.col - 1; ++current_col)
+        {
+            printf(" %7.1f,", matrix.index[current_row][current_col]);
+        }
+        printf("%7.1f ]\n", matrix.index[current_row][matrix.col - 1]);
+    }
+    printf("\n");
 }
 
-void copyMatrix(Matrix from, Matrix to)
+
+void addMatrix(Matrix base1, Matrix base2, Matrix* results)
 {
-    for(int i = 0; i < 3; ++i)
-        for(int j = 0; j < 3; ++j)
-            to[i][j] = from[i][j];
+    iniMatrix(results, base1.row, base1.col);
+    for(int current_row = 0; current_row < base1.row; ++current_row)
+        for(int current_col = 0; current_col < base1.col; ++current_col)
+            results->index[current_row][current_col] = base1.index[current_row][current_col]
+                    + base2.index [current_row][current_col];
 }
 
-void transpose(Matrix base, Matrix results)
+void subMatrix(Matrix base1, Matrix base2, Matrix* results)
 {
-    for(int i = 0; i < 3; ++i)
-        for(int j = 0; j < 3; ++j)
-            results[i][j] = base[j][i];
+    iniMatrix(results, base1.row, base1.col);
+    for(int current_row = 0; current_row < base1.row; ++current_row)
+        for(int current_col = 0; current_col < base1.col; ++current_col)
+            results->index[current_row][current_col] = base1.index[current_row][current_col]
+                    - base2.index [current_row][current_col];
+}
+
+void copyMatrix(Matrix from, Matrix* to)
+{
+    iniMatrix(to, from.row, from.col);
+    for(int current_row = 0; current_row < from.row; ++current_row)
+        for(int current_col = 0; current_col < from.col; ++current_col)
+            to->index[current_row][current_col] = from.index[current_row][current_col];
+}
+
+void multiplyMatrix(Matrix base, Matrix mult, Matrix* results)
+{
+    float buffer;
+    iniMatrix(results, base.row, mult.col);
+    for(int resultsRow = 0; resultsRow < base.row; ++resultsRow)
+    {
+        for(int resultsCol = 0; resultsCol < mult.col; ++resultsCol)
+        {
+            buffer = 0;
+            for(int index = 0; index < base.col; index++)
+            {
+                buffer = buffer + (base.index[resultsRow][index] * mult.index[index][resultsCol]);
+            }
+            results->index[resultsRow][resultsCol] = buffer;
+        }
+    }
+}
+
+void scalarMatrix(Matrix base, float scalar, Matrix* results)
+{
+    iniMatrix(results, base.row, base.col);
+    for(int current_row = 0; current_row < base.row; ++current_row)
+        for(int current_col = 0; current_col < base.col; ++current_col)
+            results->index[current_row][current_col] = base.index[current_row][current_col] * scalar;
+}
+
+void powerMatrix(Matrix base, int exponent, Matrix* results)
+{
+    Matrix bufferMatrix;
+    copyMatrix(base, &bufferMatrix);
+    copyMatrix(base, results);
+    for(int i = 1; i < exponent; ++i)
+    {
+        multiplyMatrix(bufferMatrix, base, results);
+        copyMatrix(*results, &bufferMatrix);
+    }
 }
--- main.c
+++ main.c
@@ -2,42 +2,96 @@
 
 void calculator(Matrix input)
 {
+    Matrix input2;
     Matrix resultsMatrix;
-    printf("\nCalculator Functions: \n");
-    printf("1. Transpose\n");
-    printf("2. Determinant\n");
-    printf("3. Addition / Subtraction\n");
-    printf("4. Multiplication\n");
-    printf("5. Cofactor Matrix\n");
-    printf("6. Adjunct Matrix\n");
-    printf("7. Inverse Matrix\n");
-    printf("8. Scalar Exponentiation\n");
+    printf("\n1. Addition / Subtraction\n");
+    printf("2. Multiplication\n");
+    printf("3. Scalar Multiplication\n");
+    printf("4. Scalar Exponentiation\n");
     printf("9. Exit\n");
     printf("Please select the function: ");
     int select = getint();
 
     switch(select)
     {
-        case 1:
-            transpose(input, resultsMatrix);
-            printf("The tranposed matrix is:\n");
+        case 1: //add and sub
+            printf("Add(1) or Subtract(2)?: ");
+            int addsub = getint();
+            while(addsub != 1 && addsub != 2)
+            {
+                printf("Invalid input!\n");
+                printf("Add(1) or Subtract(2)?: ");
+                addsub = getint();
+            }
+            getMatrix(&input2);
+            if(input.col != input2.col || input.row != input2.row)
+            {
+                printf("Error: cannot add or subtract matrices of different dimensions!");
+                break;
+            }
+            printf("Matrix 1 + Matrix 2");
+            printMatrix(input);
+            printMatrix(input2);
+            if(addsub == 1)
+                addMatrix(input, input2, &resultsMatrix);
+            else
+                subMatrix(input, input2, &resultsMatrix);
+            printf("The result matrix is:\n");
+            printMatrix(resultsMatrix);
+            break;
+        case 2: // multiplication
+            printf("Input the matrix you want to multiply with:\n" );
+            getMatrix(&input2);
+            if(input.col != input2.row)
+            {
+                printf("Incompatible matrix! Cannot multiply %ix%i matrix with %ix%i matrix!",
+                       input.row, input.col, input2.row, input2.col);
+                break;
+            }
+            printf("Matrix 1 x Matrix 2");
+            printMatrix(input);
+            printMatrix(input2);
+            multiplyMatrix(input, input2, &resultsMatrix);
+            printf("The result of multiplication is:\n");
+            printMatrix(resultsMatrix);
+            break;
+        case 3: // scalar multiplication
+            printf("Input a number to multiply the matrix with: ");
+            float scalar = getfloat();
+            scalarMatrix(input, scalar, &resultsMatrix);
+            printf("The matrix times %.2f\n", scalar);
+            printMatrix(input);
+            printf("equals\n");
+            printMatrix(resultsMatrix);
+            break;
+        case 4: // scalar expo
+            if(input.row != input.col)
+            {
+                printf("Matrix must be a square matrix!\n");
+                break;
+            }
+            printf("Input a positive integer exponent: ");
+            int expo;
+            for(expo = getint(); expo < 1; expo = getint())
+                printf("Invalid exponent! Please input again.\nInput a positive integer exponent: ");
+            powerMatrix(input, expo, &resultsMatrix);
+            printf("The matrix raised to the power of %i is:\n", expo);
+            printMatrix(resultsMatrix);
             break;
         case 9:
             printf("Exiting program");
             exit(0);
         default:
-            printf("Selection error\n");
+            printf("Selection error\nPlease select again\n");
             calculator(input);
     }
-    printMatrix(resultsMatrix);
 }
 
 int main(void)
 {
     Matrix matrix;
-    getMatrix(matrix);
-    printf("The matrix you just input is: \n");
+    getMatrix(&matrix);
+    printf("The matrix you just input is:\n");
     printMatrix(matrix);
-
     calculator(matrix);
 }
